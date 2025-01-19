8ac44e08945477635e324d15a86b47a6afd7dc4c
careroms <careroms.wasu.jp@gmail.com>
2025-01-19 15:43:40 +0900

    Add new sh and yaml config for kind

---
+++ kind-install-script.sh
@@ -0,0 +1,6 @@
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+kind completion bash > ./kind_completion
+sudo mv ./kind_completion /etc/bash_completion.d/kind_completion
