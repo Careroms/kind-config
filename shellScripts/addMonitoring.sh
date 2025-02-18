8a74070057f530e74ac4a363c36968913782dc3e
careroms <careroms.wasu.jp@gmail.com>
2025-02-19 07:21:59 +0900

    Change info 1. add loki cluster url and change loki images 2. Helm jenkins add uno choice

--- shellScripts/addMonitoring.sh
+++ shellScripts/addMonitoring.sh
@@ -1,2 +1,5 @@
 helm upgrade prometheus prometheus-community/kube-prometheus-stack --install 
-helm upgrade  loki --namespace=loki-stack grafana/loki-stack --install --create-namespace
+helm upgrade loki --namespace=loki-stack grafana/loki-stack \
+  --set loki.url="http://loki.loki-stack.svc.cluster.local:3100" \
+  --set loki.image.tag="2.9.0" \
+  --install --create-namespace
